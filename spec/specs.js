5936d5ff5cb9315f166880aa44b947f2e440faaf
Chris Lytsell & Jose Torres <student@epicodus.com>
2015-09-08 14:25:57 -0700

    write html for output of factorial to the screen

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('factorial', function(){
     it("is \"1\" if the number is \"0\"", function(){
         expect(factorial(0)).to.equal(1);
     });
+
+    it("returns the product of all whole numbers less than the input number", function(){
+        expect(factorial(7)).to.equal(5040);
+    });
 });
